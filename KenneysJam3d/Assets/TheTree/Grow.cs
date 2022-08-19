8ba1c355a03f2024b1c48ab7631d0b9f2b36ef3c
Hammahutz <mottz94@gmail.com>
2022-08-19 23:37:11 +0200

    Tree growing

---
+++ KenneysJam3d/Assets/TheTree/Grow.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour
+{
+    public bool hasGrown = false;
+
+    private Vector3 endScale = new Vector3(10, 10, 10);
+    private Vector3 startScale;
+    private float duration = 5f;
+    private float elapsedTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GrowTree(gameObject);
+    }
+
+    private void Addbranches()
+    {
+        var test = Instantiate(gameObject, new Vector3(transform.position.x + 2, transform.position.y + 2, transform.position.z + 2), Quaternion.identity);
+    }
+
+    private void GrowTree(GameObject gameObject)
+    {
+        if (Input.GetKey(KeyCode.G) && !hasGrown)
+        {
+            hasGrown = true;
+        }
+        if (hasGrown)
+        {
+            elapsedTime += Time.deltaTime;
+            float complete = elapsedTime / duration;
+
+            transform.localScale = Vector3.Lerp(startScale, endScale, complete);
+
+            if (elapsedTime > duration)
+            {
+                startScale = endScale;
+                endScale *= 10;
+                elapsedTime = 0;
+                hasGrown=false;
+                Addbranches();
+            }
+        }
+    }
+}
